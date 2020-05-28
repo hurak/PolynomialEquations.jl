38f484234a1160fb8a5681af3ba10e30259dc0ff
Zdenek Hurak <hurak@fel.cvut.cz>
2020-05-28 16:30:32 +0200

    Corrected runntest.jl (forgot to save it before the previous commit).

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using Test
         a = Polynomial(1:5,:s)
         ρ=10
         as = scale(a,ρ)
-        Polynomial(1 + 20*s + 300*s^2 + 4000*s^3 + 50000*s^4)
-        isequal(a,as)
+        ac = Polynomial([1,20,300,4000,50000],:s)
+        isequal(as,ac)
     end;
 end;
 
